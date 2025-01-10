cd1425543fc5e652b69c516a304fdf835709c50c
SaskiaBLittle <briesg1@student.op.ac.nz>
2025-01-10 22:29:01 +1300

    create footer component

---
+++ myapp/src/lib/Footer.svelte
@@ -0,0 +1,44 @@
+<script>
+
+</script>
+
+<div class = "footerStyle">
+    <p>
+        &copy; The information provided has been used from the referenced sites in the about/contact page for your personal use in accordance with your Lecturer Dominic Sarelius in History 210.
+    </p>
+    <nav>
+        <a href="/">Contact</a>
+        <a href="/">Accessibility</a>
+        <a href="/">How to Use</a>
+        <a href="/">Privacy Policy</a>
+        <a href="/">References</a>
+    </nav>
+</div>
+
+<style>
+    .footerStyle{   
+        border-radius: 5px;
+        background-color: rgb(9, 23, 171);
+        padding : 2em;
+        font-weight: bold;
+        font-size: large;
+        line-height: 1.5;
+        text-align: center;
+        display : flex;
+        flex-direction: column;
+        min-height: 1vh;
+        margin-top: auto;
+        color: #dddddddd;
+    }
+    nav a{
+        padding: 3px 10px;
+        text-decoration: none;
+        color:rgb(9, 23, 171);
+        flex: 1;
+    }
+    nav a:hover{
+        color: rgb(66, 143, 243);
+        background-color: #dddddddd;
+        border-radius: 5px;
+    }
+</style>
