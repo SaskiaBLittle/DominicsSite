5b5322afd2b5d1eb10c72cf0724f02d7dcd9d9fc
SaskiaBLittle <briesg1@student.op.ac.nz>
2025-01-11 10:54:30 +1300

    Layout added

---
+++ myapp/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Navigation from "$lib/Navigation.svelte";
+    import Footer from "$lib/Footer.svelte";
+</script>
+
+<Navigation />
+
+<slot />
+
+<Footer />
+
+<style>
+    :global(*){
+        font-family:Arial, Helvetica, sans-serif
+    }
+</style>
