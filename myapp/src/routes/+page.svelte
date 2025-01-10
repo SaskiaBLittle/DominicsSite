30f16fd421091b69e851b91b468c04918f0b1302
SaskiaBLittle <briesg1@student.op.ac.nz>
2025-01-10 22:29:20 +1300

    home page header and footer added

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Header from '$lib/Header.svelte';
+    import Footer from '$lib/Footer.svelte';
+</script>
+
+<Header headingTitle="HomePage" />
+
+<Footer />
+
+<style>
+
+</style>
