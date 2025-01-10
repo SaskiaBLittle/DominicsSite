d31e12c74f199526fe841f96f98f9b2562e6726d
SaskiaBLittle <briesg1@student.op.ac.nz>
2025-01-10 22:44:45 +1300

    made changes to the home page

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
     import Header from '$lib/Header.svelte';
     import Footer from '$lib/Footer.svelte';
+    import Navigation from '$lib/Navigation.svelte';
 </script>
 
 <Header headingTitle="HomePage" />
+<Navigation />
 
 <Footer />
 
