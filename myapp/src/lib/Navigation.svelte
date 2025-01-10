141e112ac39bd30885caa3ab9cc9150ab09b5952
SaskiaBLittle <briesg1@student.op.ac.nz>
2025-01-10 22:44:25 +1300

    made Navigation component

---
+++ myapp/src/lib/Navigation.svelte
@@ -0,0 +1,41 @@
+<script>
+
+</script>
+
+<section>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/historicalfigures">Historical Figures</a>
+        <a href="/kingdoms">Kingdoms</a>
+        <a href="/regions">Regions</a>
+        <a href="/about">About/Contact</a>
+    </nav>
+</section>
+
+<style>
+    section {
+        display: flex;
+        align-items: center; /* Center the image and nav vertically */
+        background-color: rgb(9, 23, 171);
+        padding: 0; /* Remove padding to fit the image */
+        border-radius: 5px;
+    }
+    nav {
+        display: flex;
+        align-items: center; /* Center the text vertically */
+        background-color: transparent; /* Make the background transparent */
+    }
+    nav a {
+        color: #ddd;
+        text-align: left; /* Align text to the left */
+        padding: 30px 30px; /* Adjust padding as needed */
+        text-decoration: none;
+        font-weight: bold;
+        font-size: large;
+        border-radius: 5px;
+    }
+    nav a:hover {
+        background-color: rgb(66, 143, 243);
+        color: rgb(9, 23, 171);
+    }
+</style>
