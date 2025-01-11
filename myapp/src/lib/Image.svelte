cb229f80962ef28e15908014a98322292d8fd94b
SaskiaBLittle <briesg1@student.op.ac.nz>
2025-01-12 09:39:52 +1300

    image component added

---
+++ myapp/src/lib/Image.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let pic;
+    export let picname;
+    export let picwidth;
+    export let picheight;
+</script>
+
+<div>
+    <img src={pic} alt={picname} width={picwidth} height={picheight}>
+</div>
+
+<style>
+
+</style>
