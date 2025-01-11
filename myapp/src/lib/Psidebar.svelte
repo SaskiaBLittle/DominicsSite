40871b3b981ca63811e751e0ee57dcaba46a5e21
SaskiaBLittle <briesg1@student.op.ac.nz>
2025-01-12 09:45:01 +1300

    Person side bar made

---
+++ myapp/src/lib/Psidebar.svelte
@@ -0,0 +1,36 @@
+<script>
+
+</script>
+
+<aside>
+    <a href="/">Person 1</a>
+    <a href="/">Person 2</a>
+    <a href="/">Person 3</a>
+    <a href="/">Person 4</a>
+</aside>
+
+<style>
+    aside {
+        margin-right: 1em;
+        background-color: rgb(9, 23, 171);
+        text-align: center;
+        padding-left: 0.5em;
+        padding-right: 0.5em;
+        border: 2px solid rgb(9, 23, 171);
+        border-radius: 5px;
+    }
+    aside a {
+        display: block;
+        color:#ddd;
+        text-align: center;
+        text-decoration: none;
+        font-weight: bold;
+        font-size: large;
+        margin: 0.7em 0;
+        border-radius: 5px;
+    }
+    aside a:hover {
+        color: rgb(9, 23, 171);
+        background-color: rgb(66, 143, 243);
+    }
+</style>
