f36bed35935822e4d017b287db8c3800e51bcf0d
SaskiaBLittle <briesg1@student.op.ac.nz>
2025-02-01 20:38:44 +1300

    Add side bar as a layout

---
+++ myapp/src/routes/historicalfigures/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Psidebar from "../../lib/Psidebar.svelte";
+</script>
+
+<div>
+    <Psidebar />
+    <main>
+        <slot />
+    </main>
+</div>
+
+<style>
+    div {
+        display: flex;
+        padding-top: 5px;
+        padding-bottom: 5px;
+    }
+</style>
